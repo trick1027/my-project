a5ed6c06f63618d016feb774b31a6ec683ec8ad1
eunhye.choi <trick1120@hanmail.net>
2021-12-20 19:48:00 +0900

    수정

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-     <h2 class="primary">git merge 이걸로</h2>
+     <h2 class="primary">git merge 이걸로 올려볼래</h2>
     <ul>
       <li><i class="ico_com level_h">level h</i></li>
     </ul>

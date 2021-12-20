b570b84127460e2fccd4513cef70124599d9ca46
eunhye.choi <trick1120@hanmail.net>
2021-12-20 19:50:22 +0900

    화면수정

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-     <h2 class="primary">git merge 이거수정했어</h2>
+     <h2 class="primary">git merge 다른거해보자</h2>
     <ul>
       <li><i class="ico_com level_h">level h</i></li>
     </ul>
